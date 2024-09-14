f61fe64f700a0e86e837225737542adcd075eca3
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-14 20:46:10 +0430

    fa icon added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import { FaBars } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa'; 
 
 const App = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -13,27 +13,25 @@ const App = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        {/* Main content */}
         <div className="col-md-9 col-lg-10">
-          {/* Burger icon for toggling sidebar */}
-          <button className="btn btn-primary mt-2" onClick={toggleSidebar}>
-            {/* <FaBars /> */}
+          <button className="btn btn-primary mt-2 burger-icon" onClick={toggleSidebar}>
+            <FaBars />
           </button>
           <div className="content p-3">
-            <h1>Main Content</h1>
-            <p>This is the main content area.</p>
-          </div>
-        </div>
-
-        {/* Sidebar */}
-        <div
-          className={`col-md-3 col-lg-2 bg-light sidebar ${sidebarOpen ? 'd-block' : 'd-none'} d-md-block`}>
           <h4 className="p-3">Sidebar</h4>
           <ul className="list-unstyled px-3">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Contact</a></li>
           </ul>
+           
+          </div>
+        </div>
+
+        {/* Sidebar */}
+        <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        <h1>Main Content</h1>
+        <p>This is the main content area.</p>
         </div>
       </div>
     </div>
