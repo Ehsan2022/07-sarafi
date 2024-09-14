11f4a07fe5b508fbe2b43a0eb53ed358b1c49ec5
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-14 22:35:49 +0430

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import { FaBars } from 'react-icons/fa'; 
+// App.js
+import React from 'react';
+import Sidebar from './Sidebar';
 
-const App = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
-
+function App() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-9 col-lg-10">
-          <button className="btn btn-primary mt-2 burger-icon" onClick={toggleSidebar}>
-            <FaBars />
-          </button>
-          <div className="content p-3">
-          <h4 className="p-3">Sidebar</h4>
-          <ul className="list-unstyled px-3">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-           
-          </div>
-        </div>
-
-        {/* Sidebar */}
-        <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+    <div className="App">
+      <Sidebar />
+      <div className="content" style={{ marginLeft: '250px', padding: '20px' }}>
         <h1>Main Content</h1>
-        <p>This is the main content area.</p>
-        </div>
+        <p>Your main content goes here.</p>
       </div>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,75 @@
+/** @format */
+
+// Sidebar.js
+import React from "react";
+import "./index.css"; // For custom styles
+
+const Sidebar = () => {
+  return (
+    <>
+      {/* Sidebar for large screens */}
+      <div className="d-none d-lg-block sidebar bg-dark text-light p-3">
+        <a className="nav-link text-light" href="#">
+          <h2 className="text-center">معامله</h2>
+        </a>
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <a className="nav-link" href="#home">
+              خانه
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
+              ثبت مشتری
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Navbar for small screens */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-lg-none">
+        <a className="navbar-brand" href="#">
+          معامله
+        </a>
+        <button
+          className="navbar-toggler open"
+          type="button"
+          data-toggle="collapse"
+          data-target="#sidebarNav"
+          aria-controls="sidebarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse" id="sidebarNav">
+          <div className="sidebar-mobile bg-dark text-light p-3">
+            <button
+              className="navbar-toggler close"
+              type="button"
+              data-toggle="collapse"
+              data-target="#sidebarNav"
+              aria-controls="sidebarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span>❌</span>
+            </button>
+            <ul className="nav flex-column pt-3">
+              <li className="nav-item">
+                <a className="nav-link" href="#home">
+                  خانه
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">
+                  ثبت مشتری
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Sidebar;
