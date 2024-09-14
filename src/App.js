afa32ec0adec13dd063a0314251ed81904f0baff
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-14 17:36:24 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        {/* Main Content */}
+        <div className="col-md-9">
+          <div className="p-3">
+            <h1>Main Content</h1>
+            <p>This is the main content area.</p>
+            <p>
+              You can add more content here, and it will take 9 columns of the
+              grid, while the sidebar takes 3 columns on the right.
+            </p>
+          </div>
+        </div>
+
+        {/* Sidebar */}
+        <div className="col-md-3 bg-light border-left">
+          <div className="p-3">
+            <h2>Sidebar</h2>
+            <ul className="list-group">
+              <li className="list-group-item">Item 1</li>
+              <li className="list-group-item">Item 2</li>
+              <li className="list-group-item">Item 3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+function SideBar() {
+  return <></>;
+}
