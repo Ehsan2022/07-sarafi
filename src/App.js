c745a769523cca96fa5f65f1f59d302ec3a82a1e
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-15 16:50:48 +0430

    customer add form , table for showing created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+/** @format */
+
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidebar from './Sidebar'; // Import Sidebar component
-import Home from './Home'; // Import Home component
-import Customer from './Customer'; // Import About component
+import React from "react";
 
-const App = () => {
+export default function App() {
   return (
-    <Router>
-      <div className="d-flex">
-        <Sidebar /> {/* Render Sidebar */}
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/customer" element={<Customer />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-};
+    <div className="body">
+      <div className="customer-form">
+        <form>
+          <h5>Add Customer</h5>
+          <label>Name</label>
+          <input type="text" />
+          <label>Phone</label>
+          <input type="text" />
+          <label>Address</label>
+          <input type="text" />
+          <label>Balance</label>
+          <input type="text" />
 
-export default App;
+          <button className="save-btn">Save</button>
+        </form>
+      </div>
+      <div className="customer-list">
+      <h5>List Of Customers</h5>
+
+        <table>
+          <tr className="table-heading">
+            <th>#</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>Balance</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Ehsan</td>
+            <td>00000000000</td>
+            <td>Herat</td>
+            <td>25678000</td>
+            <td><button className="select-customer-btn">Select</button></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
