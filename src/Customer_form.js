3ea986a2d1a3537327878994557b8e15b65ffc59
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-15 17:35:22 +0430

    a state created for each input

--- src/Customer_form.js
+++ src/Customer_form.js
@@ -1,19 +1,39 @@
 /** @format */
-import React from "react";
+import React, { useState } from "react";
 
 export default function Customer_form() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState(0);
   return (
     <div className="customer-form">
       <form>
         <h5>Add Customer</h5>
         <label>Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label>Phone</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
         <label>Address</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
         <label>Balance</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={balance}
+          onChange={(e) => setBalance(e.target.value)}
+        />
 
         <button className="save-btn">Save</button>
       </form>
