b3b922586891f08a64c3eb2f55d34ed6816c7ec7
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-15 17:21:00 +0430

    form and table divided in saprate pages

--- src/App.js
+++ src/App.js
@@ -6,47 +6,8 @@ import React from "react";
 export default function App() {
   return (
     <div className="body">
-      <div className="customer-form">
-        <form>
-          <h5>Add Customer</h5>
-          <label>Name</label>
-          <input type="text" />
-          <label>Phone</label>
-          <input type="text" />
-          <label>Address</label>
-          <input type="text" />
-          <label>Balance</label>
-          <input type="text" />
-
-          <button className="save-btn">Save</button>
-        </form>
-      </div>
-      <div className="list">
-        <table>
-          <tr className="table-heading">
-            <th>#</th>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Address</th>
-            <th>Balance</th>
-            <th>Action</th>
-          </tr>
-        </table>
-        <div className="customer-table">
-          <table>
-            <tr>
-              <td>1</td>
-              <td>Ehsan</td>
-              <td>00000000000</td>
-              <td>Herat</td>
-              <td>25678000</td>
-              <td>
-                <button className="select-customer-btn">Select</button>
-              </td>
-            </tr>{" "}
-          </table>
-        </div>
-      </div>
+      
+      
     </div>
   );
 }
---
+++ src/Customer_form.js
@@ -0,0 +1,17 @@
+/** @format */
+
+<div className="customer-form">
+  <form>
+    <h5>Add Customer</h5>
+    <label>Name</label>
+    <input type="text" />
+    <label>Phone</label>
+    <input type="text" />
+    <label>Address</label>
+    <input type="text" />
+    <label>Balance</label>
+    <input type="text" />
+
+    <button className="save-btn">Save</button>
+  </form>
+</div>;
