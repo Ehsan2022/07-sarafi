e421942464370d0e0cdc0976e3a39469f731402b
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-15 17:19:35 +0430

    list partation updated

--- src/App.js
+++ src/App.js
@@ -21,89 +21,31 @@ export default function App() {
           <button className="save-btn">Save</button>
         </form>
       </div>
-      <table>
-        <tr className="table-heading">
-          <th>#</th>
-          <th>Name</th>
-          <th>Phone</th>
-          <th>Address</th>
-          <th>Balance</th>
-          <th>Action</th>
-        </tr>
-      </table>
-      <div className="customer-list">
+      <div className="list">
         <table>
-          <tr>
-            <td>1</td>
-            <td>Ehsan</td>
-            <td>00000000000</td>
-            <td>Herat</td>
-            <td>25678000</td>
-            <td>
-              <button className="select-customer-btn">Select</button>
-            </td>
-          </tr>{" "}
-          <tr>
-            <td>1</td>
-            <td>Ehsan</td>
-            <td>00000000000</td>
-            <td>Herat</td>
-            <td>25678000</td>
-            <td>
-              <button className="select-customer-btn">Select</button>
-            </td>
-          </tr>{" "}
-          <tr>
-            <td>1</td>
-            <td>Ehsan</td>
-            <td>00000000000</td>
-            <td>Herat</td>
-            <td>25678000</td>
-            <td>
-              <button className="select-customer-btn">Select</button>
-            </td>
-          </tr>{" "}
-          <tr>
-            <td>1</td>
-            <td>Ehsan</td>
-            <td>00000000000</td>
-            <td>Herat</td>
-            <td>25678000</td>
-            <td>
-              <button className="select-customer-btn">Select</button>
-            </td>
-          </tr>{" "}
-          <tr>
-            <td>1</td>
-            <td>Ehsan</td>
-            <td>00000000000</td>
-            <td>Herat</td>
-            <td>25678000</td>
-            <td>
-              <button className="select-customer-btn">Select</button>
-            </td>
-          </tr>{" "}
-          <tr>
-            <td>1</td>
-            <td>Ehsan</td>
-            <td>00000000000</td>
-            <td>Herat</td>
-            <td>25678000</td>
-            <td>
-              <button className="select-customer-btn">Select</button>
-            </td>
-          </tr>{" "}
-          <tr>
-            <td>1</td>
-            <td>Ehsan</td>
-            <td>00000000000</td>
-            <td>Herat</td>
-            <td>25678000</td>
-            <td>
-              <button className="select-customer-btn">Select</button>
-            </td>
+          <tr className="table-heading">
+            <th>#</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>Balance</th>
+            <th>Action</th>
           </tr>
         </table>
+        <div className="customer-table">
+          <table>
+            <tr>
+              <td>1</td>
+              <td>Ehsan</td>
+              <td>00000000000</td>
+              <td>Herat</td>
+              <td>25678000</td>
+              <td>
+                <button className="select-customer-btn">Select</button>
+              </td>
+            </tr>{" "}
+          </table>
+        </div>
       </div>
     </div>
   );
